eef3681446a5a69d16dc7ea64152ef813e43df45
Sabine Pierre <sabine.pierre@gmail.com>
2017-09-06 07:48:12 -0400

    First commit ;)

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+const AlbumDetail = () => {
+    return (
+        <View>
+            <Text>Album Detail</Text>
+        </View>
+    );
+};
+
+export default AlbumDetail;
